229b552a3282f4dfe20b48513bb70bbcbf360a1b
François Kooman <fkooman@tuxed.net>
2015-12-08 10:17:01 +0100

    also sign releases, and upload them

--- make_release_tarball.sh
+++ make_release_tarball.sh
@@ -3,15 +3,18 @@ COMMIT=9222616442ddb6b1a512784901490c85a6e67016
 VERSION=0.9.8
 
 (
-  rm -rf release
-  mkdir release
-  cd release
-  git clone https://github.com/fkooman/php-remote-storage.git php-remote-storage-${VERSION}
-  cd php-remote-storage-${VERSION}
-  git checkout ${COMMIT}
-  rm -rf .git
-  composer install --no-dev
-  #cp config/server.dev.yaml.example config/server.yaml
-  cd ..
-  tar -cJf php-remote-storage-${VERSION}.tar.xz php-remote-storage-${VERSION}
+    rm -rf release
+    mkdir release
+    cd release
+    git clone https://github.com/fkooman/php-remote-storage.git php-remote-storage-${VERSION}
+    cd php-remote-storage-${VERSION}
+    git checkout ${COMMIT}
+    rm -rf .git
+    composer install --no-dev
+    #cp config/server.dev.yaml.example config/server.yaml
+    cd ..
+    tar -cJf php-remote-storage-${VERSION}.tar.xz php-remote-storage-${VERSION}
+    gpg --sign -b -a php-remote-storage-${VERSION}.tar.xz
+    remote-storage-uploader --folder php-remote-storage php-remote-storage-${VERSION}.tar.xz
+    remote-storage-uploader --folder php-remote-storage php-remote-storage-${VERSION}.tar.xz.asc
 )
